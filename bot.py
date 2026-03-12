57ccde117a367ad25bb8cf34359528ecc7c832cc
hamidovsuhrob13-boop <hamidovsuhrob13@gmail.com>
2026-03-12 09:21:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Салом! Бот кор мекунад.")
 
 def main():
+    if not BOT_TOKEN:
+        raise ValueError("BOT_TOKEN not found")
+
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
-if __name__ == "__main__":
+if name == "__main__":
     main()
